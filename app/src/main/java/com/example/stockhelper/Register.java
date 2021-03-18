86535f9a0d164a696e637520b46e71ff8f5e6a1d
DanielAnt <daniel.antkiewicz@gmail.com>
2021-03-18 02:54:11 +0100

    connected registration form to firebase auth and firebase database

--- app/src/main/java/com/example/stockhelper/Register.java
+++ app/src/main/java/com/example/stockhelper/Register.java
@@ -1,13 +1,22 @@
 package com.example.stockhelper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,12 +26,13 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
     private EditText editTextRepeatedPassword;
     private ProgressBar registerProgressBar;
     private Button registryButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        mAuth = FirebaseAuth.getInstance();
         editTextLogin = (EditText) findViewById(R.id.registerLogin);
         editTextEmail = findViewById(R.id.registerEmail);
         editTextPassword = findViewById(R.id.registerPassword);
@@ -98,7 +108,36 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
         }
 
 
-
+        registerProgressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(login, email);
+                            Log.d("FirebaseAuth", "onComplete" + task.getException().getMessage());
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(Register.this, new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(Register.this, "You have registered successfully!", Toast.LENGTH_LONG).show();
+                                        registerProgressBar.setVisibility(View.GONE);
+                                    }
+                                    else{
+                                        Toast.makeText(Register.this, "Something gone wrong! Try again!", Toast.LENGTH_LONG).show();
+                                        registerProgressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                        }
+                        else{
+                            Toast.makeText(Register.this, "Here Something gone wrong! Try again!", Toast.LENGTH_LONG).show();
+                            registerProgressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
 
 
 
