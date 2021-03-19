8d66610c953e931a3e14f0e6b0e2f0f22c492e66
DanielAnt <daniel.antkiewicz@gmail.com>
2021-03-19 16:44:09 +0100

    added trimmed company json

--- app/src/main/java/com/example/stockhelper/Search.java
+++ app/src/main/java/com/example/stockhelper/Search.java
@@ -11,8 +11,15 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.json.JSONArray;
@@ -28,23 +35,26 @@ public class Search extends AppCompatActivity {
 
     SearchView searchView;
     ListView listView;
+    ProgressBar searchProgressBar;
     Map stockSymbols = new HashMap();
+
     String[] nameList={};
-
     ArrayAdapter<String> arrayAdapter;
+    private RequestQueue mQueue;
 
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        searchProgressBar = findViewById(R.id.searchProgressBar);
 
         arrayToList();
         loadHashMapFromJson();
         searchingViewList();
+        mQueue = Volley.newRequestQueue(this);
+
     }
 
 
@@ -59,8 +69,9 @@ public class Search extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                // https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=IBBOJPT8T6NZA44K
-                Toast.makeText(Search.this, "You click -" + stockSymbols.get(adapterView.getItemAtPosition(i).toString()), Toast.LENGTH_SHORT).show();
+                jsonParse((String) stockSymbols.get(adapterView.getItemAtPosition(i).toString()));
+
+
             }
         });
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -78,9 +89,9 @@ public class Search extends AppCompatActivity {
     }
 
 
-    public void arrayToList(){
+    private void arrayToList(){
         try {
-            JSONArray jsonArray = new JSONArray(loadJSONFromAsset("CompaniesArray.json"));
+            JSONArray jsonArray = new JSONArray(loadJSONFromAsset("CompaniesArrayTrimmed.json"));
 
             String[] temporaryList=new String[jsonArray.length()];
             for(int i = 0; i <jsonArray.length(); i++){
@@ -94,7 +105,7 @@ public class Search extends AppCompatActivity {
     }
 
 
-    public String loadJSONFromAsset(String fileName) {
+    private String loadJSONFromAsset(String fileName) {
         String json = null;
         try {
             InputStream is = getAssets().open(fileName);
@@ -110,9 +121,9 @@ public class Search extends AppCompatActivity {
         return json;
     }
 
-    public void loadHashMapFromJson(){
+    private void loadHashMapFromJson(){
         try {
-            JSONArray jsonArray = new JSONArray(loadJSONFromAsset("AllCompaniesWithSymbols.json"));
+            JSONArray jsonArray = new JSONArray(loadJSONFromAsset("AllCompaniesWithSymbolsTrimmed.json"));
 
             for(int i = 0; i <jsonArray.length(); i++){
                 JSONObject companyDetail = jsonArray.getJSONObject(i);
@@ -124,6 +135,34 @@ public class Search extends AppCompatActivity {
         }
     }
 
+    private void jsonParse(String stockSymbol) {
+        System.out.println(stockSymbol);
+        String url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" +stockSymbol+ "&apikey=IBBOJPT8T6NZA44K";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject stock = response.getJSONObject("Global Quote");
+                            String symbol = stock.getString("01. symbol");
+                            String price = stock.getString("05. price");
+                            String change = stock.getString("09. change");
+                            String changeProcentage = stock.getString("10. change percent");
+                            String test = (symbol + " " + price + " " + change + " " + changeProcentage);
+                            Toast.makeText(Search.this, "You click -" + test, Toast.LENGTH_SHORT).show();
 
-
+                    } catch (JSONException e) {
+                            e.printStackTrace();
+                            System.out.println(e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
 }
+
