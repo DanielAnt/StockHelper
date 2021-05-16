f4aca201e0a00f9ecfa928f2f96ad9f4273a49bb
konx8 <konx90@gmail.com>
2021-05-17 01:45:30 +0200

    game

---
+++ app/src/main/java/com/example/stockhelper/Menu.java
@@ -0,0 +1,112 @@
+package com.example.stockhelper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Menu extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView welcomeText;
+    private Button searchButton;
+    private Button favoriteButton;
+    private Button profileButton;
+    private Button settingsButton;
+    private Button logoutButton;
+    private FirebaseAuth mAuth;
+    private String username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                username = snapshot.child("Users").child(mAuth.getCurrentUser().getUid()).child("username").getValue(String.class);
+                welcomeText.setText("Welcome " + username + "!");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        welcomeText = findViewById(R.id.menuWelcomeText);
+        searchButton = findViewById(R.id.menuSearchButton);
+        favoriteButton = findViewById(R.id.menuFavoriteButton);
+        profileButton = findViewById(R.id.menuProfileButton);
+        settingsButton = findViewById(R.id.menuSettingsButton);
+        logoutButton = findViewById(R.id.menuLogoutButton);
+
+        searchButton.setOnClickListener(this);
+        favoriteButton.setOnClickListener(this);
+        profileButton.setOnClickListener(this);
+        settingsButton.setOnClickListener(this);
+        logoutButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.menuSearchButton:
+                openSearchActivity();
+                break;
+            case R.id.menuFavoriteButton:
+                openFavActivity();
+                break;
+            case R.id.menuProfileButton:
+                openProfileActivity();
+                break;
+            case R.id.menuSettingsButton:
+                break;
+            case R.id.menuLogoutButton:
+                Toast.makeText(Menu.this, "Logged out", Toast.LENGTH_LONG).show();
+                firebaseLogout();
+                break;
+
+    }
+
+
+
+}
+
+    public void openSearchActivity(){
+        Intent searchIntent = new Intent(this, Search.class);
+        startActivity(searchIntent);
+    }
+    public void openProfileActivity() {
+        Intent profileIntent = new Intent(this,Profile.class);
+        startActivity(profileIntent);
+    }
+
+    public void openFavActivity() {
+        Intent favIntent = new Intent(this,Favorite.class);
+        startActivity(favIntent);
+    }
+
+
+    private void firebaseLogout() {
+        mAuth.signOut();
+        startActivity(new Intent(this, Login.class));
+
+    }
+}
---
+++ app/src/main/java/com/example/stockhelper/Register.java
@@ -0,0 +1,144 @@
+package com.example.stockhelper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextUsername;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextRepeatedPassword;
+    private ProgressBar registerProgressBar;
+    private Button registryButton;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        editTextUsername = findViewById(R.id.registerUsername);
+        editTextEmail = findViewById(R.id.registerEmail);
+        editTextPassword = findViewById(R.id.registerPassword);
+        editTextRepeatedPassword = findViewById(R.id.registerRepeatedPassword);
+        registerProgressBar = findViewById(R.id.registerProgressBar);
+
+        registryButton = findViewById(R.id.registerButton);
+        registryButton.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.registerButton:
+                RegisterUser();
+                break;
+        }
+    }
+
+    public void RegisterUser(){
+        String username = editTextUsername.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String repeatedPassword = editTextRepeatedPassword.getText().toString().trim();
+
+
+
+        if(username.isEmpty()){
+            editTextUsername.setError("This field is required!");
+            editTextUsername.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()){
+            editTextEmail.setError("This field is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please provide valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("This field is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(repeatedPassword.isEmpty()){
+            editTextRepeatedPassword.setError("This field is required!");
+            editTextRepeatedPassword.requestFocus();
+            return;
+        }
+
+
+
+        if(!password.equals(repeatedPassword)){
+            editTextRepeatedPassword.setError("Passwords don't match!");
+            editTextRepeatedPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editTextPassword.setError("Password must be at least 6 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+
+        registerProgressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(username, email);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(Register.this, "You have registered successfully!", Toast.LENGTH_LONG).show();
+                                        registerProgressBar.setVisibility(View.GONE);
+                                    }
+                                    else{
+                                        Toast.makeText(Register.this, "Something gone wrong! Try again!", Toast.LENGTH_LONG).show();
+                                        registerProgressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                        }
+                        else{
+                            Toast.makeText(Register.this, "Something gone wrong! Try again!", Toast.LENGTH_LONG).show();
+                            registerProgressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+
+
+    }
+
+}
